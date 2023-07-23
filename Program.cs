2b57e32938bbd146c5213d4467290648e4f62481
Tofik Abdu <tofikabdu2002@gmail.com>
2023-07-23 09:55:08 +0300

    removed (weather controller) : remove the default weather controller

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
    );
 
-//builder.Services.AddDbContext<DataContext>();
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
